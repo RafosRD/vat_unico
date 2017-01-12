51563ed91762dca918590b83934178d9d5a53066
Raul Ovalle <raul@xmarts.do>
2017-01-12 17:19:38 -0400

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Vat unico',
+    'version': '1.0.0',
+    'description': 'Pone el campo vat de res partner como unico',
+    'author': 'Raul Ovalle, raul@xmarts.do',
+    'depends': ['sale'],
+    'data': [],
+    'installable': True,
+}
---
+++ models/res_partner.py
@@ -0,0 +1,9 @@
+from openerp import models
+
+class vat_unico_res_partner(models.Model):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+
+    _sql_constraints = [
+        ('name_uniq', 'unique(vat)', u"Ya fue registrada una empresa o persona con este numero de RNC/Cédula"),
+    ]
